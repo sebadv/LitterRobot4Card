95de1fef923137fc13a1df9ad9cca085efa40731
Séba <your@email.com>
2025-05-23 13:37:25 +0200

    Move build output to root directory for HACS compatibility

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import terser from '@rollup/plugin-terser';
 export default {
   input: ['src/litter-robot4-card.ts'],
   output: {
-    dir: 'dist',
+    file: 'litter-robot4-card.js',
     format: 'es',
   },
   plugins: [
