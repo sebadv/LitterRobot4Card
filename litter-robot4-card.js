b694b571f1d6a1c5f295018f69d3afe630f36ce5
sebadv <seba@seba.gent>
2025-05-27 14:49:12 +0200

    fix: Update import to use default import for LitElement

--- litter-robot4-card.js
+++ litter-robot4-card.js
@@ -3,7 +3,7 @@
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
  */
-import { LitElement as nt } from 'lit';
+import LitElement from 'lit';
 // Translation variables
 var ht = {title: "Litter-Robot 4", litter: "Litter", waste: "Waste", full: "Full", pet_weight: "Pet Weight", hopper: "Litter Hopper"};
 var hopper_en = {enabled: "Enabled", disabled: "Disabled", empty: "Empty", motor_fault_short: "Motor Fault (Short)", motor_ot_amps: "Motor Overcurrent", motor_disconnected: "Motor Disconnected"};
@@ -38,7 +38,7 @@ try {
 } catch (t) {
   console.error("Error adding to custom cards list:", t);
 }
-class xt extends nt {
+class xt extends LitElement {
   static async getConfigElement() {
     await customElements.whenDefined("litter-robot4-editor");
     return document.createElement("litter-robot4-editor");
